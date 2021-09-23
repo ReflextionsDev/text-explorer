11cc3302a67d27a743b5010187a1ebd7a6c20a4c
ReflextionsDev <ReflextionsDev@gmail.com>
2021-09-23 15:28:21 -0400

    Added weapon requirements and bandit encounter

--- main.js
+++ main.js
@@ -12,8 +12,8 @@ const prompt = require('prompt-sync')({ sigint: true })
 
 
 let x = 0, y = 0, location
-let playing = true, weapon = false;
-let ending;
+let playing = true, weapon = false, banditsCleared = false;
+let ending = '';
 
 console.log("You awaken, uncertain of where you are. Slowly your head clears and you remember that you were beaten and robbed. Your favorite hat is gone. You stand up and swear revenge.")
 
@@ -53,8 +53,12 @@ while (playing) {
             location = "You hear horses behind you. You turn around as an arrow flies by your head. You see a group of highwaymen, and the leader is wearing your hat. They're here to finish what they started."
             playing = false;
 
-            //if has dagger, else died
-            ending = "win";
+            if (weapon === true) {
+                ending = "win";
+            }
+            else {
+                ending = "lose";
+            }
         }
     }
 
@@ -69,7 +73,8 @@ while (playing) {
             location = "You come across a large pond. You don't feel like swimming."
         }
         if (y === 1) {
-            location = "You find an unoccupied temple. Not many worshippers these days."
+            location = "You find an unoccupied temple. Not many worshippers these days. You find some kind of sacrifical dagger. Might come in handy."
+            weapon = true;
         }
         if (y === 2) {
             location = "You enter a village. You ask around about your robbers, but the townsfolk are unhelpful and generally rude."
@@ -86,14 +91,33 @@ while (playing) {
         }
         if (y === -1) {
             location = "Here lies an abandoned house. You find a small dagger hidden under a book."
+            weapon = true;
         }
         if (y === 0) {
             location = "You come accross Fol Castle. A well known location in these parts. Well known for not taking kindly to trespassers."
         }
         if (y === 1) {
-            location = "You see a fire and approach it hoping to share in the warmth. As you get closer, you realize it's surrounded by bandits. But they've already seen you. They're drunk and they're angry."
 
-            //clear if has dagger, else died
+            // if cleared
+
+            if (banditsCleared === false) {
+
+                console.log("You see a fire and approach it hoping to share in the warmth. As you get closer, you realize it's surrounded by bandits. But they've already seen you. They're drunk and they're angry.")
+
+                if (weapon === true) {
+
+                    location = "You fight off the bandits with your weapon. You're thankful to be alive, but your life means nothing without your hat, which none of these guys seem to have."
+                    banditsCleared = true
+                }
+
+                else {
+                    location = ''
+                    playing = false;
+                    ending = "lose";
+                }
+            } else {
+                location = "You come back to the bandit camp. The fire is nearly out and the bandits are still dead."
+            }
         }
         if (y === 2) {
             location = "You come across a humble magic shop. You never had the aptitude. You curse your parents for their bad genes and move on."
@@ -125,7 +149,7 @@ while (playing) {
 
 
 if (ending == 'win') {
-    console.log("Thanfully, you have a weapon at your side. In a hat-obsessed-rage you cut down your foes. ")
+    console.log("Thanfully, you have a weapon at your side. In a hat-obsessed-rage you cut down your foes. At long last, you're reunited with your trusty hat.")
 }
 else if (ending == 'lose') {
     console.log("Nobody will ever know why you ran into a battle empty handed, and unfortunately you'll never be able to explain it cause you're dead.")
